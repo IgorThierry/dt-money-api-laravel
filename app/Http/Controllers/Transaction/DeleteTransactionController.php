d344395954e6773b36a9e0a88cd7e90d1f3dd127
Igor Thierry <igorthierry15@gmail.com>
2023-10-04 17:02:17 -0300

    feat: delete transaction

--- app/Http/Controllers/Transaction/DeleteTransactionController.php
+++ app/Http/Controllers/Transaction/DeleteTransactionController.php
@@ -3,14 +3,26 @@
 namespace App\Http\Controllers\Transaction;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\Transaction;
+
 
 class DeleteTransactionController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request, $id)
+    public function __invoke($id)
     {
+        $transaction = Transaction::find($id);
+
+        if (!$transaction) {
+            return response()->json([
+                'message' => 'Transaction not found',
+            ], 404);
+        }
+
+        $transaction->delete();
+
+        return response('', 204);
     }
 }
